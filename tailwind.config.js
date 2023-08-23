24311ebfe2434c4779970e2a73e3d03769dcb8b2
Mello Birkan <mgbirkan@icloud.com>
2023-08-22 22:20:19 -0300

    `Mobile first` feito

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "white-bg": "#ece9fd",
+        indigoc: "#7158ef",
+        title: "#090039",
+        section: "rgba(10, 0, 57, 0.64)",
+      },
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
